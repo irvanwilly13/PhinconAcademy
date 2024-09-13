8cc54306c421a21ba1b59016fb0a5f0919ec3ac5
irvanwilly13 <irvanwilly9910@gmail.com>
2024-09-13 16:48:03 +0700

    update day4, class, property Observe

--- Akademik/month1/Hari-4/Lesson/MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ Akademik/month1/Hari-4/Lesson/MyPlayground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -9,7 +9,7 @@ class Patient {
     var daftarKesehatan: [String: Double]
     
     var contactPatient: [String: String] = [:]
-
+    
     init(name: String, age: Int, bodyTemperature: Double, daftarKesehatan: [String: Double]) {
         self.name = name
         self.age = age
@@ -22,26 +22,21 @@ class Patient {
         print("Nama pasien: \(self.name), Umur: \(self.age), Suhu tubuh: \(self.bodyTemperature)")
     }
     func searchContactNumber(phone: String) {
-           let filteredContacts = contactPatient.filter { $0.value.contains(phone) }
-           if filteredContacts.isEmpty {
-               print("No contacts found with the phone number containing \(phone).")
-           } else {
-               for (name, number) in filteredContacts {
-                   print("\(name): \(number)")
-               }
-           }
-       }
-    
-    func searchByPrefix(key: String) {
-            let filteredContacts = contactPatient.filter { $0.key.lowercased().hasPrefix(key.lowercased()) }
-            if filteredContacts.isEmpty {
-                print("No contacts found with the prefix \(key).")
-            } else {
-                for (name, number) in filteredContacts {
-                    print("\(name): \(number)")
-                }
+        let filteredContacts = contactPatient.filter { $0.value.contains(phone) }
+        if filteredContacts.isEmpty {
+            print("No contacts found with the phone number containing \(phone).")
+        } else {
+            for (name, number) in filteredContacts {
+                print("\(name): \(number)")
             }
         }
+    }
+    
+    func searchByPrefix(key: String) {
+        if let filteredContacts = contactPatient.first(where: { $0.key.lowercased().hasPrefix(key.lowercased()) }) {
+            print(filteredContacts)
+        }
+    }
     
     func addContact(name: String, Phone: String) {
         contactPatient[name] = Phone
@@ -64,7 +59,7 @@ class Patient {
         }
     }
 }
-    
+
 let pasien1 = Patient(name: "Dony", age: 27, bodyTemperature: 37.4, daftarKesehatan: ["Gula": 100])
 pasien1.tampilkanNama()
 pasien1.tampilkanDaftarKesehatan()
